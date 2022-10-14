794b5e439eabcd02c8bc6d46cf2681cd9d10e9b0
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2022-10-14 17:49:34 +0200

    added PersonService interface
    added PersonService interface and he holds methods
    while body of method is in PersonService class

--- spring-mongo-atlas/src/main/java/com/crni99/springmongoatlas/service/PersonService.java
+++ spring-mongo-atlas/src/main/java/com/crni99/springmongoatlas/service/PersonService.java
@@ -2,76 +2,25 @@ package com.crni99.springmongoatlas.service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.crni99.springmongoatlas.model.Person;
-import com.crni99.springmongoatlas.repository.PersonRepository;
 
-@Service
-public class PersonService {
+public interface PersonService {
 
-	@Autowired
-	private PersonRepository personRepository;
+	public Person addPerson(Person person);
 
-	// CREATE , READ , UPDATE , DELETE
+	public List<Person> findAllPersons();
 
-	public Person addPerson(Person person) {
-		person.setPersonId(UUID.randomUUID().toString().split("-")[0]);
-		return personRepository.save(person);
-	}
+	public Person getPersonById(String personId);
 
-	public List<Person> findAllPersons() {
-		return personRepository.findAll();
-	}
+	public List<Person> getByPersonGender(String gender);
 
-	public Person getPersonById(String personId) {
-		return personRepository.findById(personId).get();
-	}
+	public List<Person> findByDateBetween(LocalDate from, LocalDate to);
 
-	public List<Person> getByPersonGender(String gender) {
-		return personRepository.findPersonsByGender(gender.toUpperCase());
-	}
+	public List<Person> findByHobbies(List<String> hobbies);
 
-	public List<Person> findByDateBetween(LocalDate from, LocalDate to) {
-		return personRepository.findPersonsByDobBetween(from, to);
-	}
+	public Person updatePerson(Person personRequested);
 
-	public List<Person> findByHobbies(List<String> hobbies) {
-		return personRepository.findPersonsByHobbiesContaining(hobbies);
-	}
+	public String deletePerson(String personId);
 
-	public Person updatePerson(Person personRequested) {
-		// get the existing document from DB
-		// populate new value from request to existing object/entity/document
-		Person existingPerson = personRepository.findById(personRequested.getPersonId()).get();
-
-		if (personRequested.getFirstName() != null) {
-			existingPerson.setFirstName(personRequested.getFirstName());
-		}
-		if (personRequested.getLastName() != null) {
-			existingPerson.setLastName(personRequested.getLastName());
-		}
-		if (personRequested.getDob() != null) {
-			existingPerson.setDob(personRequested.getDob());
-		}
-		if (personRequested.getGender() != null) {
-			existingPerson.setGender(personRequested.getGender());
-		}
-		if (personRequested.getAddress() != null) {
-			existingPerson.setAddress(personRequested.getAddress());
-		}
-		if (personRequested.getHobbies() != null) {
-			existingPerson.setHobbies(personRequested.getHobbies());
-		}
-
-		return personRepository.save(existingPerson);
-	}
-
-	public String deletePerson(String personId) {
-		personRepository.deleteById(personId);
-		return personId + " person deleted from dashboard.";
-	}
 }
---
+++ spring-mongo-atlas/src/main/java/com/crni99/springmongoatlas/service/PersonServiceImpl.java
@@ -0,0 +1,83 @@
+package com.crni99.springmongoatlas.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crni99.springmongoatlas.model.Person;
+import com.crni99.springmongoatlas.repository.PersonRepository;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+
+	@Autowired
+	private PersonRepository personRepository;
+
+	@Override
+	public Person addPerson(Person person) {
+		person.setPersonId(UUID.randomUUID().toString().split("-")[0]);
+		return personRepository.save(person);
+	}
+
+	@Override
+	public List<Person> findAllPersons() {
+		return personRepository.findAll();
+	}
+
+	@Override
+	public Person getPersonById(String personId) {
+		return personRepository.findById(personId).get();
+	}
+
+	@Override
+	public List<Person> getByPersonGender(String gender) {
+		return personRepository.findPersonsByGender(gender.toUpperCase());
+	}
+
+	@Override
+	public List<Person> findByDateBetween(LocalDate from, LocalDate to) {
+		return personRepository.findPersonsByDobBetween(from, to);
+	}
+
+	@Override
+	public List<Person> findByHobbies(List<String> hobbies) {
+		return personRepository.findPersonsByHobbiesContaining(hobbies);
+	}
+
+	@Override
+	public Person updatePerson(Person personRequested) {
+		// get the existing document from DB
+		// populate new value from request to existing object/entity/document
+		Person existingPerson = personRepository.findById(personRequested.getPersonId()).get();
+
+		if (personRequested.getFirstName() != null) {
+			existingPerson.setFirstName(personRequested.getFirstName());
+		}
+		if (personRequested.getLastName() != null) {
+			existingPerson.setLastName(personRequested.getLastName());
+		}
+		if (personRequested.getDob() != null) {
+			existingPerson.setDob(personRequested.getDob());
+		}
+		if (personRequested.getGender() != null) {
+			existingPerson.setGender(personRequested.getGender());
+		}
+		if (personRequested.getAddress() != null) {
+			existingPerson.setAddress(personRequested.getAddress());
+		}
+		if (personRequested.getHobbies() != null) {
+			existingPerson.setHobbies(personRequested.getHobbies());
+		}
+
+		return personRepository.save(existingPerson);
+	}
+
+	@Override
+	public String deletePerson(String personId) {
+		personRepository.deleteById(personId);
+		return personId + " person deleted from dashboard.";
+	}
+}
